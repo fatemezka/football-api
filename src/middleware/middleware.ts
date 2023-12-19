434f38de0b89b5dc61f3be5d3642e1f695b90256
fateme zahra <fatemezahrakalantarii@gmail.com>
2023-12-19 16:44:15 +0330

    middleware

---
+++ src/middleware/middleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import { get, merge } from "lodash";
+
+import { getUserBySessionToken } from "../database/UserModel";
+
+export const isAuthenticated = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const session_token = req.cookies["FATEME-AUTH"];
+
+    // user is not logged in
+    if (!session_token) return res.sendStatus(403);
+
+    const current_user = await getUserBySessionToken(session_token);
+    if (!current_user) return res.sendStatus(403);
+
+    merge(req, { current_user });
+
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
