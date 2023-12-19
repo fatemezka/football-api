7b934e2d3b902219077e712957fdf577bef240cf
fateme zahra <fatemezahrakalantarii@gmail.com>
2023-12-20 00:14:44 +0330

    save hashed password in database

--- src/controller/user/register.ts
+++ src/controller/user/register.ts
@@ -1,33 +1,29 @@
 import { Request, Response } from "express";
+import Bcrypt, { hash } from "bcrypt";
 import { getUserByEmail, createUser } from "../../database/UserModel";
-import { authentication, random } from "../../tools/authentication";
 
 export const register = async (req: Request, res: Response) => {
   try {
     const { username, email, password } = req.body;
 
-    if (!username || !email || !password) 
-    return res.sendStatus(400);
+    if (!username || !email || !password) return res.sendStatus(400);
 
     // check email does not exist
     const existingEmail = await getUserByEmail(email);
-    if (existingEmail) 
-    return res.sendStatus(400);
+    if (existingEmail) return res.sendStatus(400);
 
-    let salt = random();
-    
+    const hashed_password = await Bcrypt.hash(
+      password,
+      process.env.BCRYPT_SALT ?? ""
+    );
+
     // create a new user
     let user = await createUser({
-        username,
-        email,
-        authentication: {
-            salt,
-            password: authentication(salt, password),
-        },
+      username,
+      email,
+      hashed_password,
     });
-
     return res.status(200).json(user).end();
-
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
